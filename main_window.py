2c7232062b924e40f5247f4b56825f4f2520dac5
Reza <reza.rr478@gmail.com>
2025-05-11 04:30:40 -0700

    fixing minor bugs

--- main_window.py
+++ main_window.py
@@ -47,7 +47,6 @@ class ExitSystemm(QLabel):
             sys.exit()
 
 
-
 class Form(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -133,12 +132,20 @@ class Form(QMainWindow):
         self.result_lbl = QLabel(self)
         self.result_lbl.setText("Null")
         self.result_lbl.move(100,250)
-        self.result_lbl.setFixedSize(100,80)
+        self.result_lbl.setFixedSize(120,80)
         self.result_lbl.setStyleSheet("font: 20px;" \
         "background-color: transparent;" \
         "color: white;" \
         "text-align: center;" \
         "padding-left: 10px")
+
+        # Created with love
+        author_lbl = QLabel(self)
+        author_lbl.setText("Created with ❤️ by Reza © 2025")
+        author_lbl.move(5,373)
+        author_lbl.setFixedSize(400,30)
+        author_lbl.setStyleSheet("color: white;" \
+        "font: 10px")
     
 
         # textBox
@@ -160,24 +167,28 @@ class Form(QMainWindow):
         "background-color:#e2dcf5;" \
         "font: 20px;")
 
-        # button
-        calc_btn = QPushButton("محاسبه",self)
-        calc_btn.setStyleSheet("font: 20px;" \
-        "font-family: B Koodak;" \
-        "background-color: #24243e;" \
-        "color: white;" \
-        "border: none;")
-        calc_btn.move(250,250)
-        calc_btn.clicked.connect(self.calculateMargin)
 
-        calc_btn = QPushButton("کپی",self)
-        calc_btn.setStyleSheet("font: 20px;" \
+        #button's style
+        style = "QPushButton#copy:pressed{background-color: #7B98C1;}" \
+        "QPushButton#copy{font: 20px;" \
         "font-family: B Koodak;" \
         "background-color: #24243e;" \
         "color: white;" \
-        "border: none;")
-        calc_btn.move(250,300)
-        calc_btn.clicked.connect(self.copyToClipboard)
+        "border: none;}"
+
+        # calculate button
+        self.calc_btn = QPushButton("محاسبه",self)
+        self.calc_btn.setObjectName("copy")
+        self.calc_btn.setStyleSheet(style)
+        self.calc_btn.move(250,250)
+        self.calc_btn.clicked.connect(self.calculateMargin)
+
+        #copt to clipboard btn
+        copy_btn = QPushButton("کپی",self)
+        copy_btn.setObjectName("copy")
+        copy_btn.setStyleSheet(style)
+        copy_btn.move(250,300)
+        copy_btn.clicked.connect(self.copyToClipboard)
 
 
     def calculateMargin(self):
@@ -186,6 +197,8 @@ class Form(QMainWindow):
         margin = self.margin_txtBox.text()
         calc = (((float(materialsize) - float(mediasize))/2)+float(margin))*10
         self.result_lbl.setText(f"{str(calc)} mm")
+        
+
 
     def copyToClipboard(self):
         pyperclip.copy(self.result_lbl.text()[:-5])
